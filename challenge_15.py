5e4b5cb6830791ccbc20b1f107bea36330dec036
Akshay Naik <akshaynk292006@gmail.com>
2026-09-11 23:35:06 +0530

    Add anagram checker functionality

---
+++ challenge_15.py
@@ -0,0 +1,9 @@
+# Challenge 15 - Anagram Checker
+
+str1 = input("Enter first string: ").lower()
+str2 = input("Enter second string: ").lower()
+
+if sorted(str1) == sorted(str2):
+    print("Anagram")
+else:
+    print("Not an anagram")
