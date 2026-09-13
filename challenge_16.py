68695d5bb1d5a1ea0f97e936f001445b163d4a96
Akshay Naik <akshaynk292006@gmail.com>
2026-09-13 23:10:23 +0530

    Add function to find common elements in two lists

---
+++ challenge_16.py
@@ -0,0 +1,12 @@
+# Challenge 16 - Find Common Elements
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [3, 4, 5, 6, 7]
+
+common = []
+
+for num in list1:
+    if num in list2 and num not in common:
+        common.append(num)
+
+print("Common elements:", common)
