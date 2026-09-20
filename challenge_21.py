9f79db56b69c05e30981eb81317f93c14630ce6d
Akshay Naik <akshaynk292006@gmail.com>
2026-09-20 22:08:16 +0530

    Implement function to find first non-repeating character

---
+++ challenge_21.py
@@ -0,0 +1,10 @@
+# Challenge 21 - Find the First Non-Repeating Character
+
+text = input("Enter a string: ")
+
+for char in text:
+    if text.count(char) == 1:
+        print("First non-repeating character:", char)
+        break
+else:
+    print("No non-repeating character found")
